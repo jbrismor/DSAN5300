ce02c2f6f59b1cec4e7ed562d1f8410962688468
wmcgloin <wtm30@georgetown.edu>
2024-04-11 22:59:49 -0400

    chore: target variable fixed for both tracks and artists

--- scripts/binning.py
+++ scripts/binning.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import os
 
-input_directory = "../data/modified_data"  
-output_directory = "../data/cleaned_data/"
+input_directory = "../data/modified_data/"  
+output_directory = "../data/clean_data/"
 
 # Create the output directory if it doesn't exist
 if not os.path.exists(output_directory):
@@ -18,5 +18,5 @@ def categorize_to_decade(year):
 # Apply the function to create a new column
 df['decade'] = df['album_release_year'].apply(categorize_to_decade)
 
-df.to_csv(os.path.join(output_directory, "artist_profiles.csv"), index=False, quotechar='"')
+df.to_csv(os.path.join(output_directory, "tracks.csv"), index=False, quotechar='"')
 print("decade column added to dataframe.")
